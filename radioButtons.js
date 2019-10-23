499d6979e4789ee468a8b945ba225029920c157f
pablo3620 <47928304+pablo3620@users.noreply.github.com>
2019-10-23 21:52:53 +1300

    previous card

--- radioButtons.js
+++ radioButtons.js
@@ -1,21 +1,22 @@
+//allows div to act like radio buttons so that only one from the classname can have data selected time
 function radioButton(number, className) {
     var options = document.getElementsByClassName(className);
     for (var i = 0; i < options.length; i++) {
         if (i == number) {
             options[i].dataset.selected = "true";
+            options[i].dataset.done = "true";
         } else {
             options[i].dataset.selected = "false";
         }
     }
 
 }
-
+//reterns the div that is selected dataType data
 function radioValue(className, dataType) {
     var options = document.getElementsByClassName(className);
     for (var i = 0; i < options.length; i++) {
-        if (options[i].dataset.selected == "true" || options[i].checked) {
+        if (options[i].dataset.selected == "true") {
             return options[i].dataset[dataType];
         }
     }
 }
-
--- script.js
+++ script.js
@@ -1,50 +1,66 @@
-// var ms = new Date().getTime() + 86400000;
-// var tomorrow = new Date(ms);
+//function that runs on the loading of the page
 function onLoad() {
+    // sets the minimum date for the calender so that the user can't book past dates
     document.getElementById("pickupDate").setAttribute("min", new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000).toISOString().split("T")[0]);
+    // appropately enables the + and - button on the number of days selecter
     count(0)
 }
-
+//function runs on every input of pickup date or number of date. if it is valid enables the button so that the user can go to the next screen
 function timesCheck() {
-    if (pickupDate.checkValidity() && NoDay.checkValidity() && NoDay.value > 0 && NoDay.value < 15) {
+    count(0)
+    if (pickupDate.checkValidity() && NoDay.checkValidity()) {
         pickupOutput.innerHTML = pickupDate.value;
+        // calculates return date so that user can make sure it is correct
         returnOutput.innerHTML = new Date(new Date(pickupDate.value).getTime() + (86400000 * NoDay.value)).toISOString().split("T")[0];
         NoDayOutput.innerHTML = NoDay.value;
         document.getElementById("timeButton").disabled = false;
         timesInfoOutput.innerHTML = "Pickup on " + pickupOutput.innerHTML + ", Return on " + returnOutput.innerHTML
-    }
-}
-
-function count(number) {
-    console.log(parseInt(NoDay.value) + parseInt(number))
-    NoDay.value = parseInt(NoDay.value) + parseInt(number);
-    if (NoDay.value >= 14) {
-        addDay.setAttribute('onclick',  '');
-    } else if (NoDay.value <= 1) {
-        subtractDay.disabled= true;
     } else {
-        addDay.setAttribute('onclick',  'count(1)');
-        subtractDay.disabled = false;
+        document.getElementById("timeButton").disabled = true;
     }
 }
-
+//enables and disables the + and - buttons if at a max or minimum so that user 
+//onclick must be changed as to keep the styling simple buttons overlap so mutible are clicked at a time
+function count(number) {
+    NoDay.value = parseInt(NoDay.value) + parseInt(number);
+    addDay.setAttribute('onclick', 'count(1)');
+    subtractDay.setAttribute('onclick', 'count(-2)');
+    NoDay.setAttribute('onclick', 'count(1)');
+    if (NoDay.value >= 14) {
+        addDay.setAttribute('onclick', '');
+    } else if (NoDay.value <= 1) {
+        subtractDay.setAttribute('onclick', 'count(-1)');
+        NoDay.setAttribute('onclick', 'count(0)');
+    }
+}
+//runs on click of one of the car card and dispalys car info and allows the user to proceed
+// could possiblely shoren as many lines are repeated
 function carModel(number) {
     radioButton(number, 'car_model');
     document.getElementById("modelButton").disabled = false;
     modelOutput.innerHTML = radioValue('car_model', 'info');
     modelInfoOutput.innerHTML = radioValue('car_model', 'info');
     carCostOutput.innerHTML = radioValue('car_model', 'price');
+    modelSelected.innerHTML = radioValue('car_model', 'info');
+    seatsSelected.innerHTML = radioValue('car_model', 'seats');
+    bodySelected.innerHTML = radioValue('car_model', 'body');
+    luggageSelected.innerHTML = radioValue('car_model', 'luggage');
+    sizeSelected.innerHTML = radioValue('car_model', 'engine');
+    usageSelected.innerHTML = radioValue('car_model', 'fuel');
+    typeSelected.innerHTML = radioValue('car_model', 'fueltype');
+    transmissionSelected.innerHTML = radioValue('car_model', 'transmission');
+    costSelected.innerHTML = radioValue('car_model', 'price')
 }
-
+//runs on selection of extras and allows the user to proceed
 function extras(number, className) {
     radioButton(number, className);
     document.getElementById("extraButton").disabled = false;
     extrasSelectedOutput.innerHTML = extrasOutput();
     extrasInfoOutput.innerHTML = extrasOutput();
-    totalCostOutput.innerHTML = parseInt(carCostOutput.innerHTML) * parseInt(NoDayOutput.innerHTML) + parseInt(insuranceCostOutput.innerHTML) + parseInt(extrasCostOutput.innerHTML) + parseInt(bookingFeeOutput.innerHTML);
+    totalCostOutput.innerHTML = parseInt(carCostOutput.innerHTML) * parseInt(NoDayOutput.innerHTML) + parseInt(insuranceCostOutput.innerHTML) * parseInt(NoDay.value) + parseInt(extrasCostOutput.innerHTML) + parseInt(bookingFeeOutput.innerHTML);
 }
 
-//function to display extras 
+//function to display selected extras as a string to make it easier for the user
 function extrasOutput() {
     var extrasInfo = "",
         cost = 0,
@@ -58,17 +74,29 @@ function extrasOutput() {
     extrasCostOutput.innerHTML = cost;
     return extrasInfo.substring(0, extrasInfo.length - 2);
 }
-
+//enables button for the user to proceed if all inputs are valid
+//also adds these these to the confimation card
 function infoCheck() {
     if (firstName.checkValidity() && lastName.checkValidity() && age.checkValidity() && phoneNo.checkValidity() && email.checkValidity()) {
         document.getElementById("infoButton").disabled = false;
+    } else {
+        document.getElementById("infoButton").disabled = true;
     }
     nameOutput.innerHTML = firstName.value + " " + lastName.value;
     ageOutput.innerHTML = age.value;
     phoneOutput.innerHTML = phoneNo.value;
     emailOutput.innerHTML = email.value;
 }
-
+//allows the user to confirm details once agrees to the terms and condtion
+function termsCheck() {
+    if (terms.checked) {
+        confirmButton.disabled = false;
+    } else {
+        confirmButton.disabled = true;
+    }
+}
+//pushs data to firebase using data in the confirmation card
+//also changes the confirmation card to a full screen booking summary
 function confirm() {
     firebase.database().ref('bookings').push({
         Name: nameOutput.innerHTML,
@@ -81,4 +109,15 @@ function confirm() {
         Extras: extrasSelectedOutput.innerHTML,
         Total_Cost: "$" + totalCostOutput.innerHTML
     });
+    final.dataset.done = true;
+    confirmInfo.innerHTML = "Booking Summary";
+    termsbox.style.display = "none";
+    confirmButton.setAttribute('onclick', 'location.reload()');
+    confirmButton.innerHTML = 'Return';
+}
+//allows user to reopen the cards if have already been opened in the case of an error
+function openCard(num, className) {
+    if (document.getElementsByClassName("content")[num].dataset.done == "true") {
+        radioButton(num, className)
+    }
 }
